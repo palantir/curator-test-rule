32301d4f15987f15923a7e602568e19926a7ae50
Justin Uang <juang@palantir.com>
2014-03-05 09:50:48 -0500

    Add logback configuration for tests

---
+++ src/test/resources/logback.groovy
@@ -0,0 +1,12 @@
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder
+import ch.qos.logback.core.ConsoleAppender
+
+import static ch.qos.logback.classic.Level.ERROR
+
+appender("STDOUT", ConsoleAppender) {
+    encoder(PatternLayoutEncoder) {
+        pattern = "%d{HH:mm:ss.SSS} [%thread] %-5level %logger{50} - %msg%n"
+    }
+}
+logger("com.palantir.curatortest", DEBUG)
+root(ERROR, ["STDOUT"])
